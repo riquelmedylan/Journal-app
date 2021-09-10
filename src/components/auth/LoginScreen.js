56b38c5d10b850b50fb228b7d57736429e63cbd0
riquelmedylan <riquelmedylan@hotmail.com>
2021-09-09 22:38:37 -0300

    cambiando los valores por default

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,57 +1,53 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom'
-import { startGoogleLogin, startLoginEmailPassword } from '../../actions/auth';
-import { useForm } from '../../hooks/useForm';
-
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { startGoogleLogin, startLoginEmailPassword } from "../../actions/auth";
+import { useForm } from "../../hooks/useForm";
 
 export const LoginScreen = () => {
-
-    const {loading} = useSelector(state => state.ui)
+    const { loading } = useSelector((state) => state.ui);
 
     const dispatch = useDispatch();
 
     const [values, handleInputChange] = useForm({
-        email:'hernando@gmail.com',
-        password: '1234567'
-    })
+        email: "",
+        password: "",
+    });
 
-    const { email,password } = values
+    const { email, password } = values;
 
-    const handleLogin = (e) =>{
-        e.preventDefault()
-        dispatch(startLoginEmailPassword( email, password))
-    }
+    const handleLogin = (e) => {
+        e.preventDefault();
+        dispatch(startLoginEmailPassword(email, password));
+    };
 
-    const handleGoogleLogin = () =>{
+    const handleGoogleLogin = () => {
         dispatch(startGoogleLogin());
-    }
+    };
 
     return (
         <>
             <h3 className="auth__title">Login</h3>
 
             <form onSubmit={handleLogin}>
-
-                <input 
+                <input
                     type="text"
                     placeholder="Email"
                     name="email"
                     className="auth__input"
                     autoComplete="off"
                     value={email}
-                    onChange={ handleInputChange }
+                    onChange={handleInputChange}
                 />
 
-                <input 
+                <input
                     type="password"
                     placeholder="Password"
                     name="password"
                     className="auth__input"
                     value={password}
-                    onChange={ handleInputChange }
+                    onChange={handleInputChange}
                 />
-
 
                 <button
                     type="submit"
@@ -61,16 +57,16 @@ export const LoginScreen = () => {
                     Login
                 </button>
 
-                
                 <div className="auth__social-networks">
                     <p>Login with social networks</p>
 
-                    <div
-                        className="google-btn"
-                        onClick={ handleGoogleLogin }
-                    >
+                    <div className="google-btn" onClick={handleGoogleLogin}>
                         <div className="google-icon-wrapper">
-                            <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+                            <img
+                                className="google-icon"
+                                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                                alt="google button"
+                            />
                         </div>
                         <p className="btn-text">
                             <b>Sign in with google</b>
@@ -78,14 +74,10 @@ export const LoginScreen = () => {
                     </div>
                 </div>
 
-                <Link 
-                    to="/auth/register"
-                    className="link"
-                >
-                    Create new account    
+                <Link to="/auth/register" className="link">
+                    Create new account
                 </Link>
-
             </form>
         </>
-    )
-}
+    );
+};
--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,62 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useForm } from '../../hooks/useForm';
-import { useDispatch, useSelector } from 'react-redux';
-import validator from 'validator';
-import { removeError, setError } from '../../actions/ui';
-import { startRegisterEmailPasswordName } from '../../actions/auth';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
+import { useDispatch, useSelector } from "react-redux";
+import validator from "validator";
+import { removeError, setError } from "../../actions/ui";
+import { startRegisterEmailPasswordName } from "../../actions/auth";
 
 export const RegisterScreen = () => {
-
     const dispatch = useDispatch();
-    const {msgError} = useSelector(state => state.ui)
+    const { msgError } = useSelector((state) => state.ui);
 
     const [values, handleInputChange] = useForm({
-        name:'Hernando',
-        email: 'hernando@gmail.com',
-        password: '1234567',
-        password2: '1234567'
-    })
+        name: "",
+        email: "",
+        password: "",
+        password2: "",
+    });
 
-    const {name,email,password,password2} = values
+    const { name, email, password, password2 } = values;
 
-    const handleRegister = (e) =>{
-        e.preventDefault()
-        if(formValid()){
-            dispatch(startRegisterEmailPasswordName(email,password,name))
+    const handleRegister = (e) => {
+        e.preventDefault();
+        if (formValid()) {
+            dispatch(startRegisterEmailPasswordName(email, password, name));
         }
-    }
+    };
 
-    const formValid = () =>{
-        if(name.trim().length === 0){
-            dispatch(setError('Name is required'))
-            return false
-        }else if(!validator.isEmail(email)){
-            dispatch(setError('Email is required'))
-            return false
-        }else if(password !== password2 || password.length < 5){
-            dispatch(setError('Password should be at least 6 characters'))
-            return false
+    const formValid = () => {
+        if (name.trim().length === 0) {
+            dispatch(setError("Name is required"));
+            return false;
+        } else if (!validator.isEmail(email)) {
+            dispatch(setError("Email is required"));
+            return false;
+        } else if (password !== password2 || password.length < 5) {
+            dispatch(setError("Password should be at least 6 characters"));
+            return false;
         }
-        dispatch( removeError() )
-        return true
-    }
+        dispatch(removeError());
+        return true;
+    };
 
     return (
         <>
             <h3 className="auth__title">Register</h3>
 
-            <form onSubmit={ handleRegister }>
+            <form onSubmit={handleRegister}>
+                {msgError && (
+                    <div className="auth__alert-error">{msgError}</div>
+                )}
 
-                {
-                    msgError && (
-                    <div className="auth__alert-error">
-                        {msgError}
-                    </div>
-                    )
-                }
-
-                <input 
+                <input
                     type="text"
                     placeholder="Name"
                     name="name"
@@ -66,7 +60,7 @@ export const RegisterScreen = () => {
                     onChange={handleInputChange}
                 />
 
-                <input 
+                <input
                     type="text"
                     placeholder="Email"
                     name="email"
@@ -76,7 +70,7 @@ export const RegisterScreen = () => {
                     onChange={handleInputChange}
                 />
 
-                <input 
+                <input
                     type="password"
                     placeholder="Password"
                     name="password"
@@ -85,7 +79,7 @@ export const RegisterScreen = () => {
                     onChange={handleInputChange}
                 />
 
-                <input 
+                <input
                     type="password"
                     placeholder="Confirm password"
                     name="password2"
@@ -94,7 +88,6 @@ export const RegisterScreen = () => {
                     onChange={handleInputChange}
                 />
 
-
                 <button
                     type="submit"
                     className="btn btn-primary btn-block mb-5"
@@ -102,14 +95,10 @@ export const RegisterScreen = () => {
                     Register
                 </button>
 
-                <Link 
-                    to="/auth/login"
-                    className="link"
-                >
+                <Link to="/auth/login" className="link">
                     Already registered?
                 </Link>
-
             </form>
         </>
-    )
-}
+    );
+};
